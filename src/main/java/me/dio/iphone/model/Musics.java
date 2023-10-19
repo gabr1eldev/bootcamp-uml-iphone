7b0c005ab298e8362817774655f67e49155f79ee
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-19 18:15:18 -0300

    Creating a Musics class

---
+++ src/main/java/me/dio/iphone/model/Musics.java
@@ -0,0 +1,16 @@
+package me.dio.iphone.model;
+
+import java.util.Map;
+
+public class Musics {
+
+    private Map<Integer,String> musics;
+
+    public Map<Integer, String> getMusics() {
+        return musics;
+    }
+
+    public void setMusics(Map<Integer, String> musics) {
+        this.musics = musics;
+    }
+}
