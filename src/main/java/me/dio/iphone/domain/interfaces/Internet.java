f5615d91169cfb22df71c22a2ce52e6b970868fe
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-19 19:12:17 -0300

    Creating an Interface to internet browser

---
+++ src/main/java/me/dio/iphone/domain/interfaces/Internet.java
@@ -0,0 +1,9 @@
+package me.dio.iphone.domain.interfaces;
+
+public interface Internet {
+    //Navegador na Internet: exibirPagina, adicionarNovaAba, atualizarPagina
+
+    public String showPage();
+    public String addNewTab();
+    public String updatePage();
+}
